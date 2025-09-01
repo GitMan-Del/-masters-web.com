1410fb93fb3ab4e82cffde8791c6c722f9088a35
“[firstname <github0921@gmail.com>
2025-09-01 17:27:32 +0300

    Changes Ag2212222

--- src/app/components/WebsitePerformanceContainer.tsx
+++ src/app/components/WebsitePerformanceContainer.tsx
@@ -215,7 +215,7 @@ export default React.memo(function WebsitePerformanceContainer() {
   }
 
   return (
-    <div className="bg-white rounded-xl p-6 shadow-sm border border-gray-200 h-full min-w-72">
+    <div className="bg-white rounded-xl p-6 shadow-sm border border-gray-200 min-w-72">
       <div className="flex items-center gap-2 mb-6">
         <div className="w-6 h-6 bg-blue-100 rounded-lg flex items-center justify-center">
           <svg
