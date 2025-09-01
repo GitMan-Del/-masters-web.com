089d16919da9af89ced73c57f47e62edf936c99c
“[firstname <github0921@gmail.com>
2025-09-01 17:32:42 +0300

    90% done

--- src/app/components/RecentPaymentsContainer.tsx
+++ src/app/components/RecentPaymentsContainer.tsx
@@ -5,7 +5,7 @@ import RecentPayments from './RecentPayments';
 
 export default React.memo(function RecentPaymentsContainer() {
   return (
-    <div className="bg-white rounded-xl shadow-sm border border-gray-200 h-full flex flex-col">
+    <div className="bg-white rounded-xl shadow-sm border border-gray-200 flex flex-col">
       {/* Header simplu pentru useri */}
       <div className="p-3 border-b border-gray-200 flex-shrink-0">
         <h3 className="text-xl text-gray-900">Recent Payments</h3>
@@ -13,7 +13,7 @@ export default React.memo(function RecentPaymentsContainer() {
       </div>
       
       {/* Recent Payments component */}
-      <div className="flex-1 overflow-y-auto overflow-x-hidden h-full">
+      <div className="flex-1 overflow-y-auto overflow-x-hidden ">
         <RecentPayments />
       </div>
     </div>
